b108bf61a874b5b7b099e16ef1dcce296be2e60a
jomom <momanyi330@gmail.com>
2025-02-22 03:14:32 +0300

    add TS & useReducer implementatio'

---
+++ React-useReducer-TS/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Tutorial from './components/Tutorial'
+
+
+function App() {
+  
+  return (
+    <div>
+      <Tutorial/>
+    </div>
+  )
+}
+
+export default App
---
+++ React-useReducer-TS/src/components/Tutorial.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { useReducer } from "react";
+
+interface State {
+  count: number;
+  error: string | null;
+}
+
+interface Action {
+  type: "increment" | "decrement";
+}
+
+function reducer(state: State, action: Action) {
+  const { type } = action;
+
+  switch (type) {
+    case "increment": {
+      const newCount = state.count + 1;
+      const hasError = newCount > 5;
+      return {
+        ...state,
+        count: hasError ? state.count : newCount,
+        error: hasError ? "Maximum count has reached!" : null,
+      };
+    }
+    case "decrement": {
+      const newCount = state.count - 1;
+      const hasError = newCount < 0;
+      return {
+        ...state,
+        count: hasError ? state.count : newCount,
+        error: hasError ? "Minimum reached!" : null,
+      };
+    }
+    default:
+      return state;
+  }
+}
+
+const Tutorial = () => {
+  const [state, dispatch] = useReducer(reducer, {
+    count: 0,
+    error: null,
+  });
+
+  return (
+    <div>
+      <div className="tutorial">
+        <div>Count: {state.count}</div>
+        {state.error && <div className="mb-2 text-red-500">{state.error}</div>}
+        <button
+          className="mb-2"
+          onClick={() => dispatch({ type: "increment" })}
+        >
+          Increment
+        </button>
+        <button onClick={() => dispatch({ type: "decrement" })}>
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Tutorial;
